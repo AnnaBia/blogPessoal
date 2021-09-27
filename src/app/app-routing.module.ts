89509186eb572eebc778ed8c6fea906b46a4a069
AnnaBia <bia_espejo@hotmail.com>
2021-09-27 14:01:31 -0300

    Traçando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+// array objeto de rotas
+const routes: Routes = [
+  {
+    path:"", redirectTo: 'entrar', pathMatch:'full'
+  },
+  {
+    path:'entrar', component: EntrarComponent
+  },
+  {
+    path:'cadastrar', component: CadastrarComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
